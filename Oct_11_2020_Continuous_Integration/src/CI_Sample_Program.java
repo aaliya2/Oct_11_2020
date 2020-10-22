ac05b5465c0619e10867be35e05657af087c4870
aaliya2 <sithara24@gmail.com>
2020-10-22 20:01:29 +0530

    Committed

---
+++ Oct_11_2020_Continuous_Integration/src/CI_Sample_Program.java
@@ -0,0 +1,12 @@
+
+public class CI_Sample_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("selenium");
+		System.out.println("automation");
+		System.out.println("webdriver");
+		System.out.println("java");
+	}
+
+}
